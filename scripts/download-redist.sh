ed24c33ab05f19b4d210c8c864cebf9e2955b993
Warren Spits <warren@advantic.com.au>
2024-10-11 20:52:23 +1100

    Corrected OS cases

--- scripts/download-redist.sh
+++ scripts/download-redist.sh
@@ -3,25 +3,25 @@ set -e
 SCRIPT=$(realpath "$0")
 
 VERSION=$1
-PLATFORM=$2
+OS=$2
 TARGET_PATH=$3
 CHECKSUMS=$(dirname "$SCRIPT")/checksums.txt
 
-case $PLATFORM in
-  linux)
+case $OS in
+  Linux)
     BASE_URL=https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/
     MQC_REDIST_ARCHIVE=${VERSION}-IBM-MQC-Redist-LinuxX64.tar.gz
     ;;
-  windows)
+  Windows)
     BASE_URL=https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/
     MQC_REDIST_ARCHIVE=${VERSION}-IBM-MQC-Redist-Win64.zip
     ;;
-  macos)
+  macOS)
     BASE_URL=https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/mactoolkit/
     MQC_REDIST_ARCHIVE=${VERSION}-IBM-MQ-DevToolkit-MacOS.pkg
     ;;
   *)
-    >&2 echo "Unsupported platform $PLATFORM"
+    >&2 echo "Unsupported OS $OS"
     exit 1
     ;;
 esac
@@ -31,7 +31,7 @@ grep $MQC_REDIST_ARCHIVE $CHECKSUMS > /dev/null || (>&2 echo "Unknown archive $M
 mkdir -p $TARGET_PATH
 cd $TARGET_PATH
 
-grep $MQC_REDIST_ARCHIVE $CHECKSUMS | sha256sum --check --status > /dev/null || \
+grep $MQC_REDIST_ARCHIVE $CHECKSUMS | sha256sum --check --status 2>&1 > /dev/null || \
   curl -o $MQC_REDIST_ARCHIVE --retry 10 --retry-connrefused --location --silent --show-error --fail ${BASE_URL}${MQC_REDIST_ARCHIVE}
 
 grep $MQC_REDIST_ARCHIVE $CHECKSUMS | sha256sum --check --status
