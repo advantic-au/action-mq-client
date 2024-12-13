8518a38085d8121ce7203de34c89864fc6fcbe34
Warren Spits <warren@advantic.com.au>
2024-12-13 22:55:17 +1100

    security static analysis (#7)
    security: shellcheck and zizmor fixes

--- scripts/download-redist.sh
+++ scripts/download-redist.sh
@@ -5,7 +5,7 @@ SCRIPT=$(realpath "$0")
 VERSION=$1
 OS=$2
 TARGET_PATH=$3
-CHECKSUMS=$(dirname "$SCRIPT")/../checksums.txt
+CHECKSUMS=$(dirname "${SCRIPT}")/../checksums.txt
 
 case $OS in
   Linux)
@@ -25,19 +25,19 @@ case $OS in
     SHA256="shasum -a 256"
     ;;
   *)
-    >&2 echo "Unsupported OS $OS"
+    >&2 echo "Unsupported OS ${OS}"
     exit 1
     ;;
 esac
 
-grep $MQC_REDIST_ARCHIVE $CHECKSUMS > /dev/null || (>&2 echo "Unknown archive $MQC_REDIST_ARCHIVE"; exit 1)
+grep "${MQC_REDIST_ARCHIVE}" "${CHECKSUMS}" > /dev/null || (>&2 echo "Unknown archive ${MQC_REDIST_ARCHIVE}"; exit 1)
 
-mkdir -p $TARGET_PATH
-cd $TARGET_PATH
+mkdir -p "${TARGET_PATH}"
+cd "${TARGET_PATH}"
 
-grep $MQC_REDIST_ARCHIVE $CHECKSUMS | $SHA256 --check --status > /dev/null 2>&1 || \
-  curl -o $MQC_REDIST_ARCHIVE --retry 10 --retry-connrefused --location --silent --show-error --fail ${BASE_URL}${MQC_REDIST_ARCHIVE}
+grep "${MQC_REDIST_ARCHIVE}" "${CHECKSUMS}" | ${SHA256} --check --status > /dev/null 2>&1 || \
+  curl -o "${MQC_REDIST_ARCHIVE}" --retry 10 --retry-connrefused --location --silent --show-error --fail "${BASE_URL}${MQC_REDIST_ARCHIVE}"
 
-grep $MQC_REDIST_ARCHIVE $CHECKSUMS | $SHA256 --check --status || (>&2 echo "File checksum validation failure"; exit 1)
+grep "${MQC_REDIST_ARCHIVE}" "${CHECKSUMS}" | ${SHA256} --check --status || (>&2 echo "File checksum validation failure"; exit 1)
 
-echo $TARGET_PATH/$MQC_REDIST_ARCHIVE
+echo "${TARGET_PATH}/${MQC_REDIST_ARCHIVE}"
