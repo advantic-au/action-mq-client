1b407d016687f8e8bca7696b907b649520255ada
Warren Spits <warren@advantic.com.au>
2025-04-11 14:36:13 +1000

    feat: full sync on checksum file, removing unavailable files

--- new-redist.py
+++ new-redist.py
@@ -4,11 +4,13 @@ import re
 import sys
 from bs4 import BeautifulSoup, SoupStrainer
 from requests.adapters import HTTPAdapter, Retry
+from urllib.parse import urljoin, unquote, urlparse
+from pathlib import PurePosixPath
 
 checksum_filename = sys.argv[1]
 
 checksum_file = open(checksum_filename, "r")
-files = set([line.rstrip().split()[-1] for line in checksum_file.readlines()])
+files = dict([tuple(reversed(line.rstrip().split())) for line in checksum_file.readlines()])
 checksum_file.close()
 
 linux_re = re.compile(".*?LinuxX64\\.tar\\.gz")
@@ -31,20 +33,27 @@ def sha256_url(url):
             m.update(chunk)
     return m.hexdigest()
 
-def new_redist_files(url, output):
-    response = requests.get(url)
-    a_links = BeautifulSoup(response.text, 'html.parser', parse_only=SoupStrainer('a'))
-    hrefs = [link["href"] for link in a_links if link.has_attr('href')]
-    redist_files = set([redist_file for redist_file in hrefs if is_redist(redist_file) ])
+def new_redist_files(url_list, output):
+    redist_url_list = [];
+    for url in url_list:
+        response = requests.get(url)
+        a_links = BeautifulSoup(response.text, 'html.parser', parse_only=SoupStrainer('a'))
+        hrefs = [link["href"] for link in a_links if link.has_attr('href')]
+        redist_url_list.extend([urljoin(url, redist_file) for redist_file in hrefs if is_redist(redist_file) ])
 
-    new_files = list(redist_files - files)
-    new_files.sort()
-
-    for file in new_files:
-        print("Downloading file for sha256: " + file, flush=True)
-        output.write(sha256_url(url + file) + "  " + file + "\n")
+    for redist_url in redist_url_list:
+        file_name = PurePosixPath(unquote(urlparse(redist_url).path)).parts[-1]
+        sha = files.get(file_name)
+        if sha == None:
+            print("Downloading file for sha256: " + file_name, flush=True)
+            sha = sha256_url(redist_url)
+        output.write(sha + "  " + file_name + "\n")
         output.flush()
 
-with open(checksum_filename, "a") as checksum_write:
-    new_redist_files("https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/", checksum_write)
-    new_redist_files("https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/mactoolkit/", checksum_write)
+redist_url = [
+    "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/?C=M;O=A",
+    "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/mactoolkit/?C=M;O=A"
+]
+
+with open(checksum_filename, "w") as checksum_write:
+    new_redist_files(redist_url, checksum_write)
