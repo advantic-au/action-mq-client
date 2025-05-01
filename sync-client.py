8437695d6ee10f8300634c5c9021dde6d2ef4921
Warren Spits <warren@advantic.com.au>
2025-05-01 22:33:26 +1000

    feat: Support Linux ARM64

--- sync-client.py
+++ sync-client.py
@@ -13,12 +13,13 @@ checksum_file = open(checksum_filename, "r")
 files = dict([tuple(reversed(line.rstrip().split())) for line in checksum_file.readlines()])
 checksum_file.close()
 
-linux_re = re.compile(".*?LinuxX64\\.tar\\.gz")
-win_re = re.compile(".*?Win64\\.zip")
-mac_re = re.compile(".*?MacOS\\.pkg")
+linux_x64_re = re.compile(".*?IBM-MQC-Redist-LinuxX64\\.tar\\.gz")
+linux_arm64_re = re.compile(".*?IBM-MQ-Advanced-for-Developers-Non-Install-LinuxARM64\\.tar\\.gz")
+win_re = re.compile(".*?IBM-MQC-Redist-Win64\\.zip")
+mac_re = re.compile(".*?IBM-MQ-DevToolkit-MacOS\\.pkg")
 
-def is_redist(file):
-    return win_re.fullmatch(file) or linux_re.fullmatch(file) or mac_re.fullmatch(file)
+def is_client_archive(file):
+    return win_re.fullmatch(file) or linux_x64_re.fullmatch(file) or linux_arm64_re.fullmatch(file) or mac_re.fullmatch(file)
 
 def sha256_url(url):
     m = hashlib.sha256()
@@ -33,27 +34,28 @@ def sha256_url(url):
             m.update(chunk)
     return m.hexdigest()
 
-def new_redist_files(url_list, output):
-    redist_url_list = [];
+def new_client_files(url_list, output):
+    client_url_list = [];
     for url in url_list:
         response = requests.get(url)
         a_links = BeautifulSoup(response.text, 'html.parser', parse_only=SoupStrainer('a'))
         hrefs = [link["href"] for link in a_links if link.has_attr('href')]
-        redist_url_list.extend([urljoin(url, redist_file) for redist_file in hrefs if is_redist(redist_file) ])
+        client_url_list.extend([urljoin(url, archive_file) for archive_file in hrefs if is_client_archive(archive_file) ])
 
-    for redist_url in redist_url_list:
-        file_name = PurePosixPath(unquote(urlparse(redist_url).path)).parts[-1]
+    for client_url in client_url_list:
+        file_name = PurePosixPath(unquote(urlparse(client_url).path)).parts[-1]
         sha = files.get(file_name)
         if sha == None:
             print("Downloading file for sha256: " + file_name, flush=True)
-            sha = sha256_url(redist_url)
+            sha = sha256_url(client_url)
         output.write(sha + "  " + file_name + "\n")
         output.flush()
 
-redist_url = [
+client_url = [
     "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/?C=M;O=A",
-    "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/mactoolkit/?C=M;O=A"
+    "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/mactoolkit/?C=M;O=A",
+    "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqadv/?C=M;O=A"
 ]
 
 with open(checksum_filename, "w") as checksum_write:
-    new_redist_files(redist_url, checksum_write)
+    new_client_files(client_url, checksum_write)
